478f99dbcf80ce502dde4e49ff893b71232299b7
nupidlo <enry94@mail.ru>
2018-11-22 14:29:19 +0300

    dj4

--- init.sh
+++ init.sh
@@ -1,7 +1,6 @@
 sudo unlink /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo ln -s /home/box/web/hello.py /etc/gunicorn.d/hello.py
-sudo gunicorn --сonfig /etc/gunicorn.d/hello.py hello
-sudo gunicorn --config /home/box/web/etc/gunicorn-django.conf ask:wsgi_application
+sudo ln -sf /home/box/web/etc/gunicorn-django.py /etc/gunicorn.d/gunicorn-django.py
+sudo gunicorn -c /home/box/web/etc/gunicorn-django.py ask.wsgi:application
 ﻿sudo /etc/init.d/mysql start
