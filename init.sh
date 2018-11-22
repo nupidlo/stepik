c638ee3a71ffea4ecd201d63253bbc791c364cc7
nupidlo <nupidlo3@gmail.com>
2018-11-22 11:12:53 +0000

    dj2

--- init.sh
+++ init.sh
@@ -2,6 +2,6 @@ sudo unlink /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo ln -s /home/box/web/hello.py /etc/gunicorn.d/hello.py
-sudo gunicorn -с /etc/gunicorn.d/hello.py hello
-sudo gunicorn -c /home/box/web/etc/gunicorn-django.conf ask.wsgi:application
+sudo gunicorn --сonfig /etc/gunicorn.d/hello.py hello
+sudo gunicorn --config /home/box/web/etc/gunicorn-django.conf ask:wsgi_application
 ﻿sudo /etc/init.d/mysql start
