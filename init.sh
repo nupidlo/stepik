7b73a8fe36597cdeedae0da3d99af76425fe93cd
nupidlo <enry94@mail.ru>
2018-11-22 14:16:36 +0300

    2222

--- init.sh
+++ init.sh
@@ -2,5 +2,5 @@ sudo unlink /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo ln -s /home/box/web/hello.py /etc/gunicorn.d/hello.py
-gunicorn -с /etc/gunicorn.d/hello.py hello
+sudo gunicorn -c /etc/gunicorn.d/hello.py hello:app
 ﻿sudo /etc/init.d/mysql start
