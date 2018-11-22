2a0c84aa7632e1ce049d5a420d32cb9bfa04574b
nupidlo <nupidlo3@gmail.com>
2018-11-22 11:05:40 +0000

    dj

--- init.sh
+++ init.sh
@@ -2,5 +2,6 @@ sudo unlink /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo ln -s /home/box/web/hello.py /etc/gunicorn.d/hello.py
-gunicorn -с /etc/gunicorn.d/hello.py hello
+sudo gunicorn -с /etc/gunicorn.d/hello.py hello
+sudo gunicorn -c /home/box/web/etc/gunicorn-django.conf ask.wsgi:application
 ﻿sudo /etc/init.d/mysql start
---
+++ question.py
@@ -0,0 +1,18 @@
+import multiprocessing
+
+bind = "0.0.0.0:8000"
+workers = multiprocessing.cpu_count() * 2 + 1
+pythonpath = '/home/box/web/ask'
+daemon = True
+errorlog = '/home/box/web/gerr.txt'
+accesslog = '/home/box/web/gacc.txt'
+
+
+def app(environ, start_response):
+	status = '200 OK'
+	headers = [
+		('Content-Type', 'text/plain')
+	]
+	resp = [bytes(i + '\n', 'ascii') for i in environ['QUERY_STRING'].split('&')]
+	start_response(status, headers)
+	return resp
